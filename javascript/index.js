3d530b10bcfbbab090d6c2993ca58334d74772a9
tezjavi@gmail.com <javiermurillocota@MacBook-Pro-de-Javier.local>
2022-08-31 22:09:52 -0600

    Añadir mas mobs

--- javascript/index.js
+++ javascript/index.js
@@ -6,7 +6,7 @@ var player1=null;
 var direction1 = 'up';
 var playerdirection1 = 'right';
 var speed = 30;
-var mobSpeed = 5;
+var mobSpeed = 10;
 var player = new Image();   
 var play = new Audio();
 var gameover = new Audio();
@@ -15,12 +15,18 @@ var mob1 = null;
 var mob2 = null;
 var mob3 = null;
 var mob4 = null;
+var mob5 = null;
+var mob6 = null;
+var mob7 = null;
+var mob8 = null;
+var mob9 = null;
 var mobs = [];
 var creeper = new Image();
 var zombie = new Image();
 var spider = new Image();
 var enderman = new Image();
 var counter = 0;
+var stop = false;
 
 
 
@@ -36,11 +42,16 @@ function start(){
 
 
     mob1 = new Cuadrado(240, 0, 50,50, 'white');
-    mob2 = new Cuadrado(340, 0, 50,50, 'white');
-    mob3 = new Cuadrado(440, 0, 50,50, 'white');
-    mob4 = new Cuadrado(590, 0, 50,50, 'white');
+    mob2 = new Cuadrado(300, 50, 50,50, 'white');
+    mob3 = new Cuadrado(360, 0, 50,50, 'white');
+    mob4 = new Cuadrado(420, 50, 50,50, 'white');
+    mob5 = new Cuadrado(480, 0, 50,50, 'white');
+    mob6 = new Cuadrado(540, 50, 50,50, 'white');
+    mob7 = new Cuadrado(600, 0, 50,50, 'white');
+    mob8 = new Cuadrado(660, 0, 50,50, 'white');
+    mob9 = new Cuadrado(720, 0, 50,50, 'white');
 
-    mobs = [mob1,mob2,mob3,mob4];
+    mobs = [mob1,mob2,mob3,mob4, mob5, mob6, mob7,mob8, mob9];
     //mobs
     creeper.src = '/src/img/mobs/creeperminecraft.jpeg';
     zombie.src = '/src/img/mobs/zombieminecraft.jpg';
@@ -61,20 +72,30 @@ function paint(){
     ctx.drawImage(zombie, mob2.x, mob2.y, 50,50);
     ctx.drawImage(spider, mob3.x, mob3.y, 50,50);
     ctx.drawImage(enderman, mob4.x, mob4.y, 50,50);
+    ctx.drawImage(creeper, mob5.x, mob5.y, 50,50);
+    ctx.drawImage(zombie, mob6.x, mob6.y, 50,50);
+    ctx.drawImage(spider, mob7.x, mob7.y, 50,50);
+    ctx.drawImage(enderman, mob8.x, mob8.y, 50,50);
+    ctx.drawImage(enderman, mob9.x, mob9.y, 50,50);
     update();
 }
 function update(){
     mobs.forEach(mob =>  {
-        counter % 2 == 0 ? mob.y += mobSpeed + generateRandomInteger(10) : mob.y += mobSpeed + generateRandomInteger(5)
-        counter ++
         if (mob.y>700){
             mob.y=0;
         }
         if(player1.se_tocan(mob)){
             speed = 0;
-            mobSpeed = 0;
+            stop=true;
             console.log("Si jala");
         }
+        if(stop){
+            mobSpeed = 0;
+        }else{
+            counter % 2 == 0 ? mob.y += mobSpeed + generateRandomInteger(10) : mob.y += mobSpeed + generateRandomInteger(5)
+            counter ++
+        }
+
     });
 }
 
