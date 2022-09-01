8acee2a833bae4598efcb734c35998000f08d826
tezjavi@gmail.com <javiermurillocota@MacBook-Pro-de-Javier.local>
2022-09-01 14:24:39 -0600

    se agrego, muerte, ganar y sonidos

--- javascript/index.js
+++ javascript/index.js
@@ -5,12 +5,16 @@ var super_y=350;
 var player1=null;
 var direction1 = 'up';
 var playerdirection1 = 'right';
-var speed = 30;
-var mobSpeed = 5;
+var speed = 50;
+var mobSpeed = 0;
 var player = new Image();   
 var play = new Audio();
+var houseImage = new Image();
+var house = null;
 var gameover = new Audio();
 var backImage = new Image();
+var ganar = false;
+var perder = false;
 var mob1 = null;
 var mob2 = null;
 var mob3 = null;
@@ -20,6 +24,7 @@ var mob6 = null;
 var mob7 = null;
 var mob8 = null;
 var mob9 = null;
+var mobs = [];
 var mobs1 = [];
 var mobs2 = [];
 var mobs3 = [];
@@ -29,49 +34,76 @@ var spider = new Image();
 var enderman = new Image();
 var counter = 0;
 var stop = false;
-
-
-
+var spiderSound = new Audio();
+var creeperSound = new Audio();
+var zombieSound = new Audio('src/audio/minecraft-zombie-sounds.mp3');
+var endermanSound = new Audio('src/audio/enderman-stare.mp3');
+var ganaste = new Audio();
+var music = new Audio('src/audio/backgroundmusic.mp3');
+var death = new Audio('src/audio/deathsound.mp3');
+var tiempoInicio = 0.5;
+var tiempo = tiempoInicio * 60;
+var min = null;
+var sec = null;
 window.addEventListener('load', start);
+
+function timeCountdown() {
+    if (tiempo != -1 && !perder) {
+        min = Math.floor(tiempo / 60);
+        sec = tiempo % 60;
+        sec = sec < 10 ? '0' + sec : sec
+        tiempo--;
+    }else{
+        perder=true;
+    }
+
+}
+setInterval(timeCountdown, 1000);
 
 function start(){
     c=document.getElementById("mygame");
     ctx=c.getContext("2d");
-    player1 = new Cuadrado(super_x, super_y, 60, 90, 'white');
-
+    player1 = new Cuadrado(super_x, super_y, 40, 65, 'red' );
+    house = new Cuadrado(1380, generateRandomInteger(600), 100,55, 'green' );
+    
     backImage.src = 'src/img/pasto.png';
     player.src = 'src/img/javiscript.png';
-
-
-    mob1 = new Cuadrado(240, 0, 50,50, 'white');
-    mob2 = new Cuadrado(350, 0, 50,50, 'white');
+    
+    // spiderSound.src = 'src/audio/spider.mp3';
+    
+    mob1 = new Cuadrado(140, 0, 50,50, 'white');
+    mob2 = new Cuadrado(300, 0, 50,50, 'white');
     mob3 = new Cuadrado(450, 0, 50,50, 'white');
-
+    
     mob4 = new Cuadrado(620, 0, 50,50, 'white');
     mob5 = new Cuadrado(790, 0, 50,50, 'white');
     mob6 = new Cuadrado(940, 0, 50,50, 'white');
-
+    
     mob7 = new Cuadrado(950, 700, 50,50, 'white');
     mob8 = new Cuadrado(1120, 700, 50,50, 'white');
     mob9 = new Cuadrado(1300, 700, 50,50, 'white');
-
+    
     mobs1 = [mob1,mob2,mob3];
     mobs2 = [mob4,mob5,mob6];
     mobs3 = [mob7,mob8,mob9];
+    
+    mobs = [mobs1,mobs2,mobs3];
     //mobs
     creeper.src = 'src/img/mobs/creeperminecraft.jpeg';
     zombie.src = 'src/img/mobs/zombieminecraft.jpg';
     spider.src = 'src/img/mobs/spiderminecraft.jpeg';
     enderman.src = 'src/img/mobs/EnderMab.jpg';
-
-
+    houseImage.src = 'src/img/House.png';
+    
+    ganaste.src = 'src/audio/xp.mp3';
+    timeCountdown();
     paint();
 }
 function paint(){
     window.requestAnimationFrame(paint)
     ctx.drawImage(backImage, 0,0,1500,700);
     //images
-    ctx.drawImage(player, player1.x, player1.y, 90,90);
+    ctx.drawImage(player, player1.x-27, player1.y-15, 90,90);
     //mobs
     ctx.drawImage(creeper, mob1.x, mob1.y, 50,50);
     ctx.drawImage(zombie, mob2.x, mob2.y, 50,50);
@@ -82,22 +114,72 @@ function paint(){
     ctx.drawImage(creeper, mob7.x, mob7.y, 50,50);
     ctx.drawImage(zombie, mob8.x, mob8.y, 50,50);
     ctx.drawImage(enderman, mob9.x, mob9.y, 50,50);
+
+    ctx.drawImage(houseImage, house.x-35, house.y-80, 170,150);
+    // house.dibujar(ctx);
+    ctx.beginPath();
+    ctx.fillStyle ='white';
+    ctx.font = "50px Modeka";
+    ctx.fillText("Contador: "+ min + sec, 550,50);
+    //ganar
+    if(ganar){
+        ctx.beginPath();
+        ctx.fillStyle= "rgba(0,0,0,0.1)";
+        ctx.fillRect(0,0,1500,700);
+        
+        ctx.beginPath();
+        ctx.fillStyle ='white';
+        ctx.font = "50px Modeka";
+        ctx.fillText("Ganaste", 700,350);
+    }
+    else{    
+        update();
+    }
+    //perder
+    if(perder){
+        ctx.beginPath();
+        ctx.fillStyle= "rgba(255,0,0,0.5)";
+        ctx.fillRect(0,0,1500,700);
+        
+        ctx.beginPath();
+        ctx.fillStyle ='white';
+        ctx.font = "50px Modeka";
+        ctx.fillText("Perdiste", 700,350);
+    }
+    else{    
+        update();
+    }
     update();
 }
 function update(){
+    //sounds
+    // zombieSound.play();
+    // endermanSound.play();
+    music.play();
+    if (player1.se_tocan(house)){
+        speed = 0;
+        ganar = true;
+        ganaste.play();
+    }
+    mobs.forEach(mobarray => {
+        mobarray.forEach(mob =>{
+            if(player1.se_tocan(mob)){
+                speed = 0;
+                stop=true;
+                perder=true;
+                console.log("Perdiste");
+                death.play();
+            }
+        })
+    });
     mobs1.forEach(mob =>  {
         if (mob.y>700){
             mob.y=0;
         }
-        if(player1.se_tocan(mob)){
-            speed = 0;
-            stop=true;
-            console.log("Si jala");
-        }
         if(stop){
             mobSpeed = 0;
         }else{
-            counter % 2 == 0 ? mob.y += mobSpeed + generateRandomInteger(1) : mob.y += mobSpeed + generateRandomInteger(20);
+            counter % 2 == 0 ? mob.y += mobSpeed + generateRandomInteger(1) : mob.y += mobSpeed + generateRandomInteger(8);
             counter ++;
         }
     });
@@ -105,15 +187,10 @@ function update(){
         if (mob.y<0){
             mob.y=700;
         }
-        if(player1.se_tocan(mob)){
-            speed = 0;
-            stop=true;
-            console.log("Si jala");
-        }
         if(stop){
             mobSpeed = 0;
         }else{
-            counter % 2 == 0 ? mob.y -= mobSpeed + generateRandomInteger(1) : mob.y -= mobSpeed + generateRandomInteger(20);
+            counter % 2 == 0 ? mob.y -= mobSpeed + generateRandomInteger(1) : mob.y -= mobSpeed + generateRandomInteger(8);
             counter ++;
         }
     });
@@ -121,15 +198,10 @@ function update(){
         if (mob.y>700){
             mob.y=0;
         }
-        if(player1.se_tocan(mob)){
-            speed = 0;
-            stop=true;
-            console.log("Si jala");
-        }
         if(stop){
             mobSpeed = 0;
         }else{
-            counter % 2 == 0 ? mob.y += mobSpeed + generateRandomInteger(1) : mob.y += mobSpeed + generateRandomInteger(20);
+            counter % 2 == 0 ? mob.y += mobSpeed + generateRandomInteger(1) : mob.y += mobSpeed + generateRandomInteger(8);
             counter ++;
         }
     });
@@ -137,10 +209,11 @@ function update(){
 
 
 document.addEventListener('keydown', function (e) {
-    console.log(e);
 // Arriba
     if(e.keyCode==87 || e.keyCode==38){
+        direction = 'up';
         player1.y -= speed;
+
     }
 // Abajo
     if(e.keyCode==83 || e.keyCode==40){
